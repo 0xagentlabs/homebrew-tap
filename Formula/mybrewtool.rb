b4999340f9b3ac9b33e0a722c7a6eaa446440f18
cevin <qixingyue@gmail.com>
2025-12-28 11:51:21 +0800

    add example

---
+++ Formula/mybrewtool.rb
@@ -0,0 +1,20 @@
+# brew install --formula 0xagentlabs/tap/example
+class Mybrewtool < Formula
+    desc "Mybrewtool CLI"
+    homepage "https://mybrewtool.com"
+    version "0.1.7"
+  
+    on_arm do
+      url "https://mybrewtool.com/example_0.1.7_darwin_arm64.tar.gz"
+      sha256 "ARM_SHA256"
+    end
+  ·
+    on_intel do
+      url "https://mybrewtool.com/example_0.1.7_darwin_x86_64.tar.gz"
+      sha256 "INTEL_SHA256"
+    end
+  
+    def install
+      bin.install "mybrewtool"
+    end
+  end
